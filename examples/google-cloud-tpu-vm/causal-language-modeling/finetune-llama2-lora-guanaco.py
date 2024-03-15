e3041ad1745ec284fdd8c97ff5e793c68e1d6052
Shubham Krishna <shubhamkrishna.ism@gmail.com>
2024-03-15 08:06:26 +0000

    Change paramters, add example for LLama

--- examples/google-cloud-tpu-vm/causal-language-modeling/finetune-llama2-lora-guanaco.py
+++ examples/google-cloud-tpu-vm/causal-language-modeling/finetune-llama2-lora-guanaco.py
@@ -10,26 +10,8 @@ from trl import SFTTrainer
 
 
 def train_gemma(args):
-    raw_dataset = load_dataset("databricks/databricks-dolly-15k", split="train")
-    model_id = "google/gemma-2b"
-
-    def format_dolly(sample):
-        instruction = f"### Instruction\n{sample['instruction']}"
-        context = (
-            f"### Context\n{sample['context']}" if len(sample["context"]) > 0 else None
-        )
-        response = f"### Answer\n{sample['response']}"
-        # join all the parts together
-        prompt = "\n\n".join(
-            [i for i in [instruction, context, response] if i is not None]
-        )
-        sample["text"] = prompt
-        return sample
-
-    # apply prompt template
-    format_dataset = raw_dataset.map(
-        format_dolly, remove_columns=list(raw_dataset.features)
-    )
+    raw_dataset = load_dataset("timdettmers/openassistant-guanaco", split="train")
+    model_id = "meta-llama/Llama-2-7b-hf"
 
     # Load Tokenizer
     tokenizer = AutoTokenizer.from_pretrained(model_id)
@@ -41,7 +23,6 @@ def train_gemma(args):
     model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.bfloat16)
     lora_config = LoraConfig(
         r=8,
-        target_modules="all-linear",
         task_type=TaskType.CAUSAL_LM,
         lora_alpha=16,
         lora_dropout=0.05,
@@ -50,7 +31,7 @@ def train_gemma(args):
     # Set up the FSDP config. To enable FSDP via SPMD, set xla_fsdp_v2 to True.
     fsdp_config = {
         "fsdp_transformer_layer_cls_to_wrap": [
-            "GemmaDecoderLayer"  # Specify the layer to wrap according to the model's config
+            "LlamaDecoderLayer"  # Specify the layer to wrap according to the model's config
         ],
         "xla": True,
         "xla_fsdp_v2": True,
@@ -82,7 +63,7 @@ def train_gemma(args):
         args=training_args,
         dataset_text_field="text",
         packing=True,
-        train_dataset=format_dataset,
+        train_dataset=raw_dataset,
         tokenizer=tokenizer,
     )
     # Train the model
@@ -93,7 +74,7 @@ def train_gemma(args):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--num_epochs", default=3, type=int)
-    parser.add_argument("--train_batch_size", default=32, type=int)
+    parser.add_argument("--train_batch_size", default=16, type=int)
     parser.add_argument("--lr", default=3e-4, type=float)
     parser.add_argument("--save_steps", default=100, type=int)
     parser.add_argument("--logging_steps", default=1, type=int)
