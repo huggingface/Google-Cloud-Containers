cfab42c71e005a7482cf2e9815e17a6a058d1999
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2024-06-19 12:14:05 +0200

    Update `scripts/upload_model_to_gcs.sh`
    - Include a comment with the requirements needed to run the script
    - Run formatting

--- scripts/upload_model_to_gcs.sh
+++ scripts/upload_model_to_gcs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # This script downloads a model from HuggingFace and uploads it to a bucket in Google Cloud Storage.
+# Requirements:
+# `huggingface_hub`, `hf_transfer`, `gcloud`, and `gsutil` installed and authenticated
+# Usage:
 # ./upload_model_to_gcs.sh --model-id meta-llama/Meta-Llama-3-8B-Instruct --gcs gs://hf-models-gke-bucket/Meta-Llama-3-8B-Instruct --location us-central1
 
 # Exit immediately if a command exits with a non-zero status
@@ -8,20 +11,32 @@ set -e
 
 # Parse command-line arguments for repository and bucket
 while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        --model-id) REPOSITORY_ID="$2"; shift ;;
-        --gcs) GCS_BUCKET="$2"; shift ;;
-        # optional
-        --location) LOCATION="$2"; shift ;;
-        *) echo "Unknown parameter passed: $1"; exit 1 ;;
-    esac
-    shift
+	case $1 in
+	--model-id)
+		REPOSITORY_ID="$2"
+		shift
+		;;
+	--gcs)
+		GCS_BUCKET="$2"
+		shift
+		;;
+	# optional
+	--location)
+		LOCATION="$2"
+		shift
+		;;
+	*)
+		echo "Unknown parameter passed: $1"
+		exit 1
+		;;
+	esac
+	shift
 done
 
 # Check if necessary parameters are provided
 if [[ -z "$REPOSITORY_ID" || -z "$GCS_BUCKET" ]]; then
-    echo "Usage: $0 --model-id id --gcs gs://bucket-name"
-    exit 1
+	echo "Usage: $0 --model-id id --gcs gs://bucket-name"
+	exit 1
 fi
 
 # Ensure the necessary environment variables are set
@@ -36,8 +51,8 @@ mkdir -p $LOCAL_DIR
 echo "Downloading hf.co/$REPOSITORY_ID model files to $LOCAL_DIR..."
 huggingface-cli download $REPOSITORY_ID --exclude "*.bin" "*.pth" "*.gguf" ".gitattributes" --local-dir $LOCAL_DIR
 if [ $? -ne 0 ]; then
-    echo "Download failed with error code $?"
-    exit 1
+	echo "Download failed with error code $?"
+	exit 1
 fi
 echo "Download successfully completed!"
 
@@ -45,18 +60,18 @@ echo "Download successfully completed!"
 BUCKET_NAME=$(echo $GCS_BUCKET | cut -d'/' -f3)
 # Check if the bucket exists, if not create it
 if [ -z "$(gsutil ls | grep gs://$BUCKET_NAME)" ]; then
-    echo "Bucket gs://$BUCKET_NAME does not exist, so creating it now..."
-    if [ -z "$LOCATION" ]; then
-        gcloud storage buckets create gs://$BUCKET_NAME --default-storage-class=STANDARD --uniform-bucket-level-access
-    else
-        gcloud storage buckets create gs://$BUCKET_NAME --location=$LOCATION --default-storage-class=STANDARD --uniform-bucket-level-access
-    fi
+	echo "Bucket gs://$BUCKET_NAME does not exist, so creating it now..."
+	if [ -z "$LOCATION" ]; then
+		gcloud storage buckets create gs://$BUCKET_NAME --default-storage-class=STANDARD --uniform-bucket-level-access
+	else
+		gcloud storage buckets create gs://$BUCKET_NAME --location=$LOCATION --default-storage-class=STANDARD --uniform-bucket-level-access
+	fi
 
-    if [ $? -ne 0 ]; then
-        echo "Bucket creation failed with error code $?"
-        exit 1
-    fi
-    echo "Bucket $BUCKET_NAME created successfully!"
+	if [ $? -ne 0 ]; then
+		echo "Bucket creation failed with error code $?"
+		exit 1
+	fi
+	echo "Bucket $BUCKET_NAME created successfully!"
 fi
 
 # Upload the downloaded models to Google Cloud Storage
