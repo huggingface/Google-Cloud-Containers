9cc24369cba2680a0f7acc6907ace710841ba9de
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2024-08-07 12:50:04 +0200

    Patch `huggingface_inference_toolkit` wheel and update PyTorch Inference containers (#63)
    * Fix `containers/pytorch/inference/{cpu,gpu}`
    
    There was an issue with the previous wheel as the download of the
    artifacts from Google Cloud Storage (GCS) was not working as expected,
    but it has already been fixed and patched; as well as included the
    `google` extra to ease the installation within the `Dockerfile` to only
    rely on the package extras instead of installing the dependencies
    separately.
    
    * Update paths to `whl` and `entrypoint.sh`

--- containers/pytorch/inference/gpu/2.2.2/transformers/4.41.1/py311/entrypoint.sh
+++ containers/pytorch/inference/gpu/2.2.2/transformers/4.41.1/py311/entrypoint.sh
@@ -5,7 +5,7 @@ PORT=5000
 
 # Check if AIP_MODE is set and adjust the port for Vertex AI
 if [[ ! -z "${AIP_MODE}" ]]; then
-  PORT=${AIP_HTTP_PORT}
+    PORT=${AIP_HTTP_PORT}
 fi
 
 # Start the server
